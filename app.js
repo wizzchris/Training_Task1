fbcb8314f20f2c1ec0912907542683f2492651d3
admin <admin@DAS01179.local>
2020-09-17 16:16:26 +0100

    adds data correctly

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
 var router = express.Router();
+var mongoose = require("mongoose");
+var bodyParser = require('body-parser');
 
 
 app.set('view engine', 'pug');
@@ -13,10 +15,30 @@ app.get('/', function(req, res) {
 app.get('/database', function(req, res){
    res.render('database');
 });
-app.post('/database', (req, res) => {
-    res.json(req.body);
+
+mongoose.Promise = global.Promise;mongoose.connect("mongodb://localhost:27017/mydatabase");
+
+var nameSchema = new mongoose.Schema({
+ firstName: String,
+ lastName: String
 });
 
+var User = mongoose.model("User", nameSchema);
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post("/addname", (req, res) => {
+ var myData = new User(req.body);
+ myData.save()
+ .then(item => {
+ res.send("item saved to database");
+ })
+ .catch(err => {
+ res.status(400).send("unable to save to database");
+ });
+});
 app.listen(3000, function() {
   console.log('App listening on port 3000!');
 });
