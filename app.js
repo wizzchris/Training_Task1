227e88ec8246ad2c39c2a19ba8f0c87d0e9af56d
admin <admin@DAS01179.local>
2020-09-17 14:32:49 +0100

    starting the database

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var app = express();
+var router = express.Router();
+
 
 app.set('view engine', 'pug');
 app.set('views','./views');
@@ -11,7 +13,11 @@ app.get('/', function(req, res) {
 app.get('/database', function(req, res){
    res.render('database');
 });
+app.post('/database', (req, res) => {
+    res.json(req.body);
+});
 
 app.listen(3000, function() {
   console.log('App listening on port 3000!');
 });
+module.exports = router;
